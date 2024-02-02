d48a166c4100f69fe4b5028728a2e341aabd2a7d
Rodrigo Almeida <82787131+II2DII@users.noreply.github.com>
2024-02-02 12:26:33 -0300

    Estudos em AI

---
+++ main.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+resposta = cliente.chat.completions.create(
+    messages=[
+        {
+            "role":"system",
+            "content":"Listar apenas os nomes dos produtos, sem descrição."
+        },
+        {
+            "role":"user",
+            "content":"Liste 3 produtos sustentáveis"
+        }
+    ],
+    model="gpt-3.5-turbo-1106"
+)
+print(resposta.choices[0].message.content)
